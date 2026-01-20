a996b29abc9ebf6a89e2e4495359570888fd2531
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-01-20 20:39:17 +0530

    task Completed

---
+++ nested-for-task/js/script.js
@@ -0,0 +1,228 @@
+//task=1
+debugger;
+for (let r = 1; r <= 10; r++) {
+    if (r % 2 == 0) {
+        document.writeln('0')
+    } else {
+        document.writeln('1')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+
+//task=2
+debugger;
+for (let r = 1; r <= 10; r++) {
+    if (r % 2 != 0) {
+        document.writeln('0')
+    } else {
+        document.writeln('1')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=3
+debugger;
+for (let r = 1; r <= 10; r++) {
+    if (r % 2 == 0) {
+        document.writeln('O')
+    } else {
+        document.writeln('E')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=4
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= 5; c++) {
+        document.writeln('*')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=5
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= 5; c++) {
+        document.writeln(r)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=6
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= 5; c++) {
+        document.writeln(c)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+
+//task=7
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln(c)
+    }
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//task=8
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln(r)
+    }
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+
+
+//task=9
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln('*')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=10
+debugger;
+for (let r = 5; r >= 1; r--) {
+    for (let c = 5; c >= r; c--) {
+        document.writeln(r)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=11
+debugger;
+for (let r = 5; r >= 1; r--) {
+    for (let c = 5; c >= r; c--) {
+        document.writeln(c)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=12
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 5; c >= r; c--) {
+        document.writeln(c)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task=13
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 5; c >= r; c--) {
+        document.writeln(r)
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+
+
+//task=14
+debugger;
+for (let r = 1; r <= 5; r++) {
+    for (let c = 5; c >= r; c--) {
+        document.writeln('*')
+    }
+    document.writeln('<br>')
+
+}
+document.writeln('<br>')
+
+//task-15
+for (let r = 5; r >= 1; r--) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln("&nbsp;&nbsp;")
+    }
+
+    for (let k = r; k <= 5; k++) {
+        document.writeln(r)
+    }
+
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//task-16
+for (let r = 5; r >= 1; r--) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln("&nbsp;&nbsp;")
+    }
+
+    for (let k = r; k <= 5; k++) {
+        document.writeln('#')
+    }
+
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//task-17
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln("&nbsp;&nbsp;")
+    }
+
+    for (let k = r; k <= 5; k++) {
+        document.writeln('*')
+    }
+
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//task-18
+for (let r = 1; r <= 5; r++) {
+    for (let c = 1; c <= r; c++) {
+        document.writeln("&nbsp;&nbsp;")
+    }
+
+    for (let k = r; k <= 5; k++) {
+        document.writeln(r)
+    }
+
+    document.writeln("<br>")
+}
+document.writeln("<br>")
+
+//task-19
+for (let r = 1; r <= 5; r++) {
+    let k = (r * 2) - 1
+    for (let c = 1; c <= k; c++) {
+        document.writeln("*")
+    }
+    document.writeln("<br>")
+
+}
+
