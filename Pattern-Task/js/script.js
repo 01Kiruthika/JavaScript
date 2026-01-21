f4b046e6fce2f33fdeacb858dc2655dd52b03aa9
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-01-21 11:44:24 +0530

    Update

---
+++ Pattern-Task/js/script.js
@@ -0,0 +1,73 @@
+// //task=01
+// debugger;
+// let count=1
+// for(let r=1;r<=5;r++){
+//     for(let c=1;c<=5;c++){
+//         document.writeln(count)
+//         count++
+//     }
+//     document.writeln('<br>')
+// }
+
+// //task=02
+// debugger;
+// let Count=1
+// for(let r=1;r<=5;r++){
+//     for(let c=1;c<=r;c++){
+//         document.writeln(Count)
+//         Count++
+//     }
+//     document.writeln('<br>')
+// }
+
+//task=03
+debugger;
+for (let r = 5; r >= 1; r--) 
+{
+    let k = (r * 2) - 1
+    for (let c = 1; c <= r; c++) {
+        document.writeln("&nbsp&nbsp")
+    }
+
+    for(let i=k;i<=7;i++){
+        document.writeln(r)
+    }
+    document.writeln("<br>")
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+    //task-03
+    // debugger;
+    // for (let r = 5; r >= 1; r--) {
+    //     let k = (r * 2) - 1
+    //     for (let c = 1; c <= r; c++) {
+    //         document.writeln("&nbsp&nbsp")
+    //     }
+
+    //     for(let i=k;i<=5;i++){
+    //         document.writeln(i)
+    //     }
+    //     document.writeln("<br>")
+
+    // }
+    // //task=09
+    // debugger;
+    // for(let r=1;r<=5;r++){
+    //     for(let c=1;c<=r;c++){
+    //         document.writeln(r)
+    //     }
+
+    //     document.writeln('<br>')
+    // }
