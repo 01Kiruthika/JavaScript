54f3aa32cdb5d1edc48c22ec180f2afac1e7dc99
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-01-28 12:31:49 +0530

    File Addded

---
+++ Seminar-Practice/js/math.js
@@ -0,0 +1,20 @@
+export default function addition(a,b){
+    let z = a+b   
+    return z
+}
+
+export  function Subtraction(a,b){
+    let z = a-b   
+    return z
+}
+
+export function multiplication(a,b){
+    let z = a*b   
+    return z
+}
+
+export function Division(a,b){
+    let z = a/b   
+    return z
+}
+
---
+++ Seminar-Practice/js/script.js
@@ -0,0 +1,14 @@
+import add ,{Subtraction,multiplication,Division} from "./math.js"
+//import {subtraction} from "./math.js"
+
+let num = add(10,20)
+console.log(num)
+
+let sub = Subtraction(50,30)
+console.log(sub)
+
+let mul = multiplication(5,30)
+console.log(mul)
+
+let div = Division(30,5)
+console.log(div)
