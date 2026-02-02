7ed74d40e4f0b94fdae4da5d46e9fc424349565a
01Kiruthika <kiruthikasundaram2004@gmail.com>
2026-02-02 11:16:21 +0530

    file update

--- DOM-Task1/js/script.js
+++ DOM-Task1/js/script.js
@@ -5,7 +5,7 @@ let atc = document.getElementById("product-count")
 let addtocart = () => {
     debugger;
 
-    if (count <= 10) {
+    if (count < 10) {
         count++
         atc.innerText = count;
     } else {
@@ -15,8 +15,8 @@ let addtocart = () => {
 
 let addtocartdec = () => {
     debugger;
-    if (count < 0) {
-        alert("Negative Numbers!!")
+    if (count <= 0) {
+        alert("Negative Numbers not accepted!!")
     } else {
         count--
         atc.innerText = count;
@@ -28,3 +28,33 @@ let restbtn = () => {
     atc.innerText = 0;
     count = atc.innerText
 }
+
+let nav = document.getElementById("nav-bar")
+nav.style.border = "2px solid black"
+nav.style.backgroundColor= "green"
+nav.style.position= "relative"
+atc.style.color = "white"
+atc.style.textAlign = "end"
+
+
+let img1 = document.getElementById("imgs")
+img1.setAttribute("width","300px")
+img1.setAttribute("height","300px")
+img1.style.margin = "20px"
+
+let flow = document.getElementById("btn")
+flow.style.textAlign = "center"
+flow.style.margin = "20px"
+flow.style.border = "2px solid"
+
+let bt = document.getElementById("flo")
+bt.style.margin = "20px"
+
+let ico = document.getElementById("icon")
+ico.style.width="40px"
+ico.style.height="40px"
+ico.style.position = "absolute"
+ico.style.right = "22px"
+ico.style.bottom = "20px"
+
+
